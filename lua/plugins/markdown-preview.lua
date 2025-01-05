ccd45be285740a028197e3b8fe00ec53bc12d416
Taufiq Kelrey <kelreytaufiq08@mail.ugm.ac.id>
2025-01-05 20:54:36 +0700

    change theme for markdown preview from dark to light

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,9 +1,10 @@
 return { -- Makrdown Preview
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && yarn install",
+  build = "cd app && npm install",
   init = function()
     vim.g.mkdp_filetypes = { "markdown" }
+    vim.g.mkdp_theme = "light"
   end,
   ft = { "markdown" },
 }
