1f6b42d7afceccb157847d36ba237c623884b39d
Taufiq Kelrey <kelreytaufiq@mail.ugm.ac.id>
2025-05-13 23:09:32 +0700

    guarding the dev.lua

--- lua/reys/plugins/dev.lua
+++ lua/reys/plugins/dev.lua
@@ -1,10 +1,16 @@
-return {
-    {
-        dir = "~/Kelreys/nvim-plugins/present.nvim",
-        config = function()
-            require("present").setup({
-                name = "Rey"
-            })
-        end
-    },
-}
+if vim.loop.os_uname().sysname ~= "Linux" then
+    return {
+            {
+                dir = "~/Kelreys/nvim-plugins/present.nvim",
+                config = function()
+                    require("present").setup({
+                        name = "Rey"
+                    })
+                end
+            },
+    }
+else
+    return {
+        {},
+    }
+end
