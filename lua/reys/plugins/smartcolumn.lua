f34b2b798215b4ef5379e36a21c1064b99ad1f90
Taufiq Kelrey <kelreytaufiq@mail.ugm.ac.id>
2025-05-14 17:11:54 +0700

    added python and latex to the dissabled option for smartcolumnm

--- lua/reys/plugins/smartcolumn.lua
+++ lua/reys/plugins/smartcolumn.lua
@@ -2,7 +2,7 @@ return {
   "m4xshen/smartcolumn.nvim",
   opts = {
     colorcolumn = "80",
-    disabled_filetypes = { "help", "text", "markdown" },
+    disabled_filetypes = { "help", "python", "latex", "text", "markdown" },
     custom_colorcolumn = {},
     scope = "file",
     editorconfig = true,
