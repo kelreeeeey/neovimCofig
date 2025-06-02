41745f0405e49c6f9c4fea6796f097d5682f34db
kelreeeeey <kelreytaufiq08@mail.ugm.ac.id>
2025-06-03 02:38:11 +0700

    add macros

--- lua/macros.lua
+++ lua/macros.lua
@@ -1,3 +1,5 @@
 local esc = vim.api.nvim_replace_termcodes( "<Esc>", true, true, true)
 
 vim.fn.setreg("j", "vipJVgq")
+
+vim.fn.setreg("f", "o\begin{figure}[ht!]\centering\includegraphics[]{}\end{figure}" .. esc)
